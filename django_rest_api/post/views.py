3e774cb76c1e1c6963371413a682bb3afa620fb8
oereo <dlstpgns0406@gmail.com>
2021-04-29 19:33:09 +0900

    feat: add Post api and rename Serializer

--- django_rest_api/post/views.py
+++ django_rest_api/post/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+
+class TodoView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    authentication_classes = []   #이거 두줄은 권한이 없는 상태에서 데이테 요청을 가능하게
+    permission_classes = []       #만듬 settings.py에서도 아마 가능할 것 같음.
+
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer_data = PostSerializer(data=request.data)
+        if serializer_data.is_valid():
+            serializer_data.save()
+            return Response(serializer_data.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', serializer_data.errors)
+            return Response(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
