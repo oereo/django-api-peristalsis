30da311886d29f8a58d1b733424b6618205b8310
oereo <dlstpgns0406@gmail.com>
2021-04-29 16:36:41 +0900

    feat: add managers.py and User, Profile model

--- django_rest_api/accounts/models.py
+++ django_rest_api/accounts/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.dispatch import receiver
 
 from accounts.managers import UserDefaultManager
+from django.db.models.signals import post_save
 
 
 class User(AbstractBaseUser):
@@ -47,6 +49,22 @@ class User(AbstractBaseUser):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_pk = models.IntegerField(blank=True)
+    email = models.EmailField(max_length=500, blank=True)
+    mygit = models.CharField(max_length=50, blank=True)
+    nickname = models.CharField(max_length=200, blank=True)
+    photo = models.ImageField(upload_to="profile/image", default='red.jpg')
+    myInfo = models.CharField(max_length=150, blank=True)
 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, user_pk=instance.id)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
