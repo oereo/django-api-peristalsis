873521848d2f4c9918ad982fd2b49e602f518a1b
oereo <dlstpgns0406@gmail.com>
2021-04-29 19:33:09 +0900

    feat: add Post api and rename Serializer

--- django_rest_api/post/serializers.py
+++ django_rest_api/post/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 
-class TodoSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
