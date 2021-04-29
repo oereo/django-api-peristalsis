1596dc46c67848f4f8ecb7e88e1fcebd8d272870
oereo <dlstpgns0406@gmail.com>
2021-04-29 17:14:25 +0900

    feat: setting urls.py

--- django_rest_api/django_rest_api/urls.py
+++ django_rest_api/django_rest_api/urls.py
@@ -1,21 +1,20 @@
-"""django_rest_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+from django_rest_api.global_utils.jwt_token import validate_jwt_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    path('validate/', validate_jwt_token),
+    path('login/', obtain_jwt_token),
+
+    path('verify/', verify_jwt_token),
+    path('refresh/', refresh_jwt_token),
+
+    path('user/', include('accounts.urls')),
 ]
+urlpatterns += \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
